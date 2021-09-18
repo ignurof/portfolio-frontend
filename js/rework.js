830514a3a339388226861a20502ba8dc1e05704d
ignurof <ignurof@outlook.com>
2021-09-18 19:06:18 +0200

    Changed to my personal style that I like doing

--- js/rework.js
+++ js/rework.js
@@ -1,10 +1,5 @@
-// Method that prints out input string
-const Print = function(str){
-    console.log(str);
+// Method that prints out debug value. USAGE: onclick="Debug('Hello World')"
+const Debug = (value) => {
+    console.log(value);
 }
 
-// Declare and initialize variable that is local to the scope, in this case the whole thing
-// Reference a HTML-element with the id "test"
-let test = document.getElementById("test");
-// Adds an event listener to the HTML-element that listens to clicks, and calls the appropriate method when it happens
-test.addEventListener("click", Print);
